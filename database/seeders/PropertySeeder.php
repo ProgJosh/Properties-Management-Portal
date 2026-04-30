aa9019501b31bb247059f9b8a5ba2fd5601c6c49
ProgJosh <Joshua27emmanuel30@gmail.com>
2026-05-01 00:31:44 +0800

    Ensure PropertySeeder runs

--- database/seeders/PropertySeeder.php
+++ database/seeders/PropertySeeder.php
@@ -8,39 +8,39 @@ use App\Models\Property;
 
 class PropertySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run()
     {
         Property::create([
             'landlord_id' => 1,
             'name' => 'DENIELLE LEEN APARTMENT',
+            'description' => 'A clean and affordable apartment in San Miguel',
             'address' => 'San Miguel',
             'barangay' => 'San Miguel',
             'price' => 10000,
             'status' => 1,
-            'bedroom' => 2
+            'bedroom' => 2,
         ]);
 
         Property::create([
             'landlord_id' => 1,
             'name' => 'GUIAO APARTMENT',
+            'description' => 'A 2 story Apartment roadside with parking space',
             'address' => 'San Juan Bautista',
             'barangay' => 'San Juan Bautista',
             'price' => 5000,
             'status' => 1,
-            'bedroom' => 1
+            'bedroom' => 1,
         ]);
 
         Property::create([
             'landlord_id' => 1,
             'name' => 'GOZUM APARTMENT',
+            'description' => 'Along sideroad with parking, near Chapel and Convenience Store',
             'address' => 'San Juan Nepomuceno',
             'barangay' => 'San Juan Nepomuceno',
             'price' => 8000,
             'status' => 1,
-            'bedroom' => 2
+            'bedroom' => 2,
         ]);
     }
 }
