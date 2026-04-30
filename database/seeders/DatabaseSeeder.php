2fecfad67eeafbdec35c70f936717a2a73f77e2e
ProgJosh <Joshua27emmanuel30@gmail.com>
2026-04-30 23:50:31 +0800

    Ensure PropertySeeder runs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminSeeder::class,
-            LandlordSeeder::class,
             PropertySeeder::class,
         ]);
     }
